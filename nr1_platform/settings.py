52a093b49e175766aef9c3d8a67d865ee78cb125
RiqueAlvess <henrique.alves.siqueira@hotmail.com>
2026-01-01 13:26:31 -0700

    Add files via upload

--- nr1_platform/settings.py
+++ nr1_platform/settings.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 from decouple import config
 import os
+from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
@@ -57,10 +60,11 @@ WSGI_APPLICATION = 'nr1_platform.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 # Password validation
